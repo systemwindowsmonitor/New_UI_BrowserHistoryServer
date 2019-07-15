1f1818c21d0d22dd708256ea71ec017cc9b810ca
vlalin <vlinskiy.ua@gmail.com>
2019-07-15 19:42:24 +0300

    Была добавлена новая UI c окнов логирования

---
+++ BrowserHistoryServer/BrowserHistoryServer/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BrowserHistoryServer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void GridMovement_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
